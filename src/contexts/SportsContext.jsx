1f36c277761da2ba2303bdb6345b04d610bb2699
joshuamumbere <joshuamumbere71@gmail.com>
2026-08-12 15:33:22 +0300

    chore: cumulative updates

--- src/contexts/SportsContext.jsx
+++ src/contexts/SportsContext.jsx
@@ -338,15 +338,10 @@ export const SportsProvider = ({ children }) => {
         event.kickoff_time,
 
       market:
-        selection.market ||
-        selection.market_name ||
-        selection.marketName ||
-        "Unknown Market",
+        selection.market_name,
 
       label:
-        selection.label ||
-        selection.bet ||
-        "",
+        selection.label
     };
 
     //selecting the same odd
